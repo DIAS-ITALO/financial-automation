26bd3e50ae55ed09a6aadff6bea521307cd4b63e
Italo-Diass <italogabrielsiqueira02@gmail.com>
2024-07-09 22:02:31 -0300

    feat worksheet assignment

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ planilha_clientes = openpyxl.load_workbook('dados_clientes.xlsx')
 pagina_clientes = planilha_clientes['Sheet1']
 
 driver = webdriver.Chrome()
-    driver.get('https://consultcpf-devaprender.netlify.app/')
+driver.get('https://consultcpf-devaprender.netlify.app/')
 
 for linha in pagina_clientes.iter_rows(min_row=2, values_only=True):
     nome, valor, cpf, vencimento = linha
@@ -17,15 +17,32 @@ for linha in pagina_clientes.iter_rows(min_row=2, values_only=True):
     sleep(5)
     campo_pesquisa = driver.find_element(By.XPATH, "//input[@id='cpfInput']")
     sleep(1)
+    campo_pesquisa.clear()
+    sleep(1)
     campo_pesquisa.send_keys(cpf)
     sleep(1)
     botao_pesquisar = driver.find_element(By.XPATH, "//button[@class='btn btn-custom btn-lg btn-block mt-3']")
     sleep(1)
     botao_pesquisar.click() 
     sleep(4)   
-    status = driver.find_element(By.XPATH, "//span[@id='statusLabel']")
-    if status.text = 'em dia' :
-        driver.find_element(By.TEXT, ")
-    else:
-        ####
     
+    status = driver.find_element(By.XPATH, "//span[@id='statusLabel']")
+    
+    if status.text == 'em dia' :
+        data_pagamento = driver.find_element(By.XPATH,"//p[@id='paymentDate']")
+        metodo_pagamento = driver.find_element(By.XPATH,"//p[@id='paymentMethod']")
+        data_pagamento_limpo = data_pagamento.text.split()[3]
+        metodo_pagamento_limpo = metodo_pagamento.text.split()[3]
+        
+        planilha_fechamento = openpyxl.load_workbook('planilha_fechamento.xlsx')
+        pagina_fechamento = planilha_fechamento['Plan1']
+        
+        pagina_fechamento.append([nome, valor, cpf, vencimento, 'em dia', data_pagamento_limpo, metodo_pagamento_limpo])
+    
+        planilha_fechamento.save('planilha_fechamento.xlsx')
+    else:
+        planilha_fechamento = openpyxl.load_workbook('planilha_fechamento.xlsx')
+        pagina_fechamento = planilha_fechamento['Plan1']
+  
+        pagina_fechamento.append([nome, valor, cpf, vencimento, 'pendente'])      
+        planilha_fechamento.save('planilha_fechamento.xlsx')
